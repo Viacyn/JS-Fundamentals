ef304de14910a2dbf1d46af5858082ae96c50f03
Osei Viacyn Kweku Ofori <addokweku524@gmai.com==-->
2025-07-13 16:50:02 +0000

    Write a script that prints a square

---
+++ 8-square.js
@@ -0,0 +1,10 @@
+const { argv } = require('node:process');
+const size = Number(argv[2]);
+if (isNaN(size)){
+  console.log('Missing size');
+}else{
+  for (let i = 0; i < size; i++){
+    console.log('X'.repeat(size));
+  }  
+}
+
